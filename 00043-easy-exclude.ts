b048b3657c9e931ebead04c072088aea69277342
ManfredHu <manfredHuFE@gmail.com>
2022-07-22 13:27:59 +0800

    feat: init

---
+++ 00043-easy-exclude.ts
@@ -0,0 +1,12 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, Exclude<'a' | 'b' | 'c', 'a'>>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, Exclude<'a' | 'b' | 'c', 'a' | 'b'>>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, Exclude<string | number | (() => void), Function>>>,
+]
+
+
+// ============= Your Code Here =============
+type MyExclude<T, U> = any
