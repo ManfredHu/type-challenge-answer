74c4b35f821cbb1033c0d76ae9d250f213d6b9c7
ManfredHu <manfredHuFE@gmail.com>
2022-08-01 20:42:43 +0800

    feat: add some medium

---
+++ 00010-medium-tuple-to-union.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>,
+]
+
+
+// ============= Your Code Here =============
+type TupleToUnion<T extends any[]> = T[number]
+
+// 知识点
+// 1. 元组tuple和数组类似，都可以用 T[number] 得到所有的子项
---
+++ 00012-medium-chainable-options.ts
@@ -0,0 +1,45 @@
+// ============= Test Cases =============
+import type { Alike, Expect } from './test-utils'
+
+declare const a: Chainable
+
+const result1 = a
+  .option('foo', 123)
+  .option('bar', { value: 'Hello World' })
+  .option('name', 'type-challenges')
+  .get()
+
+const result2 = a
+  .option('name', 'another name')
+  // @ts-expect-error
+  .option('name', 'last name')
+  .get()
+
+type cases = [
+  Expect<Alike<typeof result1, Expected1>>,
+  Expect<Alike<typeof result2, Expected2>>,
+]
+
+type Expected1 = {
+  foo: number
+  bar: {
+    value: string
+  }
+  name: string
+}
+
+type Expected2 = {
+  name: string
+}
+
+
+// ============= Your Code Here ============= 
+type Chainable<T = {}> = {
+  option: <K extends string, V>(key: K extends keyof T ? never: K, value: V) => Chainable<T & Record<K, V>>
+  get: () => T
+}
+
+// 知识点
+// 1. 可以使用 T = {} 来作为默认值，甚至默认参数与默认返回值，再通过递归传递 T 即可实现递归全局记录
+// 2. option是一个函数接收两个值，K 和 V. 为了约束key不可重复必须范型传入，Value是任意类型范型不做约束直接透传
+// 3. 因为key不可重复，很自然 K extends keyof T ? never: K 实现传入相同key报错
