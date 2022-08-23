c36cfde3050ae5ee5953b058a4827fbbca227d2c
ManfredHu <manfredHuFE@gmail.com>
2022-08-23 18:20:49 +0800

    feat: add permutation

--- questions/00009-medium-deep-readonly.ts
+++ questions/00009-medium-deep-readonly.ts
@@ -1,79 +1,80 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
-type cases = [
-  Expect<Equal<DeepReadonly<X>, Expected>>,
-]
+type cases = [Expect<Equal<DeepReadonly<X>, Expected>>];
 
 type X = {
-  a: () => 22
-  b: string
+  a: () => 22;
+  b: string;
   c: {
-    d: boolean
+    d: boolean;
     e: {
       g: {
         h: {
-          i: true
-          j: 'string'
-        }
-        k: 'hello'
-      }
+          i: true;
+          j: "string";
+        };
+        k: "hello";
+      };
       l: [
-        'hi',
+        "hi",
         {
-          m: ['hey']
-        },
-      ]
-    }
-  }
-}
+          m: ["hey"];
+        }
+      ];
+    };
+  };
+};
 
 type Expected = {
-  readonly a: () => 22
-  readonly b: string
+  readonly a: () => 22;
+  readonly b: string;
   readonly c: {
-    readonly d: boolean
+    readonly d: boolean;
     readonly e: {
       readonly g: {
         readonly h: {
-          readonly i: true
-          readonly j: 'string'
-        }
-        readonly k: 'hello'
-      }
+          readonly i: true;
+          readonly j: "string";
+        };
+        readonly k: "hello";
+      };
       readonly l: readonly [
-        'hi',
+        "hi",
         {
-          readonly m: readonly ['hey']
-        },
-      ]
-    }
-  }
-}
-
+          readonly m: readonly ["hey"];
+        }
+      ];
+    };
+  };
+};
 
 // ============= Your Code Here =============
 type DeepReadonly<T> = {
-  readonly [P in keyof T]: keyof T[P] extends never ? T[P]: DeepReadonly<T[P]>
-}
+  readonly [P in keyof T]: keyof T[P] extends never ? T[P] : DeepReadonly<T[P]>;
+};
 
 // 知识点
 // 1. Function extends Object, 这与JS里(() => {}) instanceof Object一样，但是
-type TestObject<T> = T extends Object ? true: false
-type case2 = [
-  Expect<Equal<TestObject<() => {}>, true>>,
-]
+type TestObject<T> = T extends Object ? true : false;
+type case2 = [Expect<Equal<TestObject<() => {}>, true>>];
 // 2. extends never
-type TestNever<T> = T extends never ? true: false
-type TestKeyofNever<T> = keyof T extends never ? true: false
+// https://stackoverflow.com/questions/68693054/what-is-extends-never-used-for
+type TestNever<T> = T extends never ? true : false;
+type TestKeyofNever<T> = keyof T extends never ? true : false;
 type case3 = [
   Expect<Equal<TestNever<123>, false>>, // 123 extends never，是false
-  Expect<Equal<TestNever<()=>{}>, false>>, // () => {} extends never，是false
-  Expect<Equal<TestKeyofNever<()=>{}>, true>>, // keyof () => {} extends never，是true。可以理解为function没有key
-]
+  Expect<Equal<TestNever<() => {}>, false>>, // () => {} extends never，是false
+  Expect<Equal<TestNever<never>, never>>, // tips: 
+  Expect<Equal<TestKeyofNever<() => {}>, true>>, // keyof () => {} extends never，是true。可以理解为function没有key
+  Expect<Equal<TestKeyofNever<{ a: string }>, false>>, // false, 对象有key a
+  Expect<Equal<TestKeyofNever<{}>, true>>, // true，空对象没有key
+  Expect<Equal<TestKeyofNever<object>, true>>, // true，空对象没有key
+  Expect<Equal<TestKeyofNever<string>, false>>, // false, string下有方法 number | typeof Symbol.iterator | "toString" | "charAt" | "charCodeAt" | "concat" | "indexOf" | "lastIndexOf" | "localeCompare" | "match" | "replace" | "search" | "slice"
+  Expect<Equal<TestKeyofNever<{ a: string } | { b: string }>, true>>, // union类型 没有公共的key
+  Expect<Equal<TestKeyofNever<{ b: string } | { b: number }>, false>> // union类型有公共的key 
+];
+
 // 2.1 keyof () => {} is never
-type TestFunKeyof<T> = keyof T
-type TestFunKeyofTest = TestFunKeyof<() => {}> 
-
-
-
+type TestFunKeyof<T> = keyof T;
+type TestFunKeyofTest = TestFunKeyof<() => {}>;
---
+++ questions/00296-medium-permutation.ts
@@ -0,0 +1,37 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<never>, []>>,
+]
+
+
+// ============= Your Code Here =============
+type Permutation<T, U = T> = [T] extends [never] 
+  ? []
+  : T extends U
+    ? [T, ...Permutation<Exclude<U, T>>]
+    : never
+
+
+// 知识点
+namespace t00296 {
+  // 1. tuple to array 
+  type Uni = 'A' | 'B' | 'C'
+  type S1<U> = U extends any ? [U] : never
+  type P1 = S1<Uni> // ['A']|['B']|['C']
+
+  // 2. 递归, 终结递归条件
+  // [A, ...[B, ...[C, ...Permutation<never>]]]
+  // [B, ...[A, ...[C, ...Permutation<never>]]]
+  // [C, ...[A, ...[B, ...Permutation<never>]]]
+  // 最后结果会在 Permutation<never> 下终结，但是 never 无法 extends 自己
+  type isNeverTemp<T> = T extends never ? true: false
+  type P2 = isNeverTemp<never>
+  // 所以要用下面的方式
+  type isNever<T> = [T] extends [never] ? true: false
+  type P3 = isNever<never>
+}
