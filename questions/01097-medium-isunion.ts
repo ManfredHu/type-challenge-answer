3adc78709bafd55c06241f0277f50c546d3174e4
ManfredHu <manfredHuFE@gmail.com>
2022-09-14 11:53:05 +0800

    feat: add repalcekeys

---
+++ questions/01097-medium-isunion.ts
@@ -0,0 +1,40 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<"a" | "b" | "c" | "d">, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | "">, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | "a">, false>>
+];
+
+// ============= Your Code Here =============
+type IsUnion<A, B = A> = A extends B ? ( [B] extends [A] ? false: true) : false
+
+// 知识点
+namespace t1097 {
+  type b = IsUnion<"a" | "b" | "c" | 1>;
+  // 1. [string | number] vs string | number
+  type P1 = "1" extends [string | number] ? true : false; // false, '1' not string array or number array
+  type P2 = ["1"] extends [string | number] ? true : false; // true, ['1'] is string array
+  type P3 = string | number;
+  // 2. union type's distribution
+  // 下面写法union type每次都会被传入T，最后合并所有值，所以P5 = 1 | '2'
+  type P4<T, U = T> = T extends U ? T : 0;
+  type P5 = P4<1 | "2">;
+
+  type P6 = 1 extends (1 | '2') ? true: false // true, 1 在 (1|'2')里
+  type P7 = [1 | '2'] extends [1] ? true: false // false
+  type P8 = [1 | '2'] extends ['2'] ? true: false // false
+  // 3. 上述答案执行流程
+  // 对于 IsUnion<string> 这样的，首先 string extends string 为true，即 进入 [string] extends [string] 也为真，返回false
+  // 对于 IsUnion<string | number> 这样的，执行 string extends string | number 为真，进入 [string | number] extends [string] 为假，但是这时为 union，返回true
+}
---
+++ questions/01130-medium-replacekeys.ts
@@ -0,0 +1,74 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type NodeA = {
+  type: 'A'
+  name: string
+  flag: number
+}
+
+type NodeB = {
+  type: 'B'
+  id: number
+  flag: number
+}
+
+type NodeC = {
+  type: 'C'
+  name: string
+  flag: number
+}
+
+type ReplacedNodeA = {
+  type: 'A'
+  name: number
+  flag: string
+}
+
+type ReplacedNodeB = {
+  type: 'B'
+  id: number
+  flag: string
+}
+
+type ReplacedNodeC = {
+  type: 'C'
+  name: number
+  flag: string
+}
+
+type NoNameNodeA = {
+  type: 'A'
+  flag: number
+  name: never
+}
+
+type NoNameNodeC = {
+  type: 'C'
+  flag: number
+  name: never
+}
+
+type Nodes = NodeA | NodeB | NodeC
+type ReplacedNodes = ReplacedNodeA | ReplacedNodeB | ReplacedNodeC
+type NodesNoName = NoNameNodeA | NoNameNodeC | NodeB
+
+type cases = [
+  Expect<Equal<ReplaceKeys<Nodes, 'name' | 'flag', { name: number; flag: string }>, ReplacedNodes>>,
+  Expect<Equal<ReplaceKeys<Nodes, 'name', { aa: number }>, NodesNoName>>,
+]
+
+
+// ============= Your Code Here =============
+type ReplaceKeys<U, Keys, Target> = {
+  [K in keyof U]: K extends Keys ? ( K extends keyof Target ? Target[K]: never) : U[K]
+}
+
+// 解析
+namespace t1130 {
+// 1. 迭代 U 里的每个 key，如果在Keys里，则替换为 Target 的类型。这里Nodes是union类型，所以 [K in keyof U] 里 K 是 type | name | flag | id 
+// type 和 id 在第一个case里被排除，返回 Target的 { name: number; flag: string }，第一个case结束
+// 2. 第二个case里，虽然 name 命中了 union type's distribution，但是不在 Target 里，Target只有 aa 这个 key，此时按照题意要返回 name: never，
+// 所以 Target 里没有 key 的时候要返回never
+}
+
