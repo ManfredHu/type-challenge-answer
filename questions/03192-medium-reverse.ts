6fc8cf29a124be11be662a02e3192c79ce07433e
ManfredHu <manfredHuFE@gmail.com>
2022-10-12 23:41:07 +0800

    perf: reverse better answer

--- questions/03192-medium-reverse.ts
+++ questions/03192-medium-reverse.ts
@@ -9,6 +9,9 @@ type cases = [
 
 
 // ============= Your Code Here =============
-type Reverse<T, K extends any[] = []> = T extends [infer F, ...infer Rest] ? Reverse<Rest, [F, ...K]>: K
+type Reverse<T extends any[]> = T extends [...infer Rest, infer End] ? [End, ...Reverse<Rest>]: T
 
-// 
+// 知识点
+// 1. 很容易得到如下答案，但是多了一个 K 作为临时变量存储
+// type Reverse<T, K extends any[] = []> = T extends [infer F, ...infer Rest] ? Reverse<Rest, [F, ...K]>: K
+// 2. 删除 K 得到更加精简的答案。主要是利用了 ...Reverse<Rest> 
