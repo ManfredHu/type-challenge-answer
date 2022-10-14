3c29c4d0c76303d3613878464c37ed32b5766740
ManfredHu <manfredHuFE@gmail.com>
2022-10-14 19:04:51 +0800

    feat: flip arguments

--- questions/03196-medium-flip-arguments.ts
+++ questions/03196-medium-flip-arguments.ts
@@ -25,12 +25,16 @@ type FlipArguments<T extends Function> = T extends (...args: infer Args) => infe
   (...args: Reverse<Args>) => R
 ): T
 
-type b = FlipArguments<(arg0: string, arg1: number, arg2: boolean) => void>
 // 知识点
 namespace t3196 {
   // 1. 判断范型是函数
   type t1<T> = T extends Function ? true: false
   type t2 = t1<() => {}>
   type t3 = t1<'string'>
-  // 2. 参数位置翻转，很容易联想到 Reverse
+  // 2. 参数类型按照位置翻转，很容易联想到 Reverse，但是注意这里只是对函数参数类型做了 Reverse，函数参数 args0 到 args2 是没变的
+  type t4<T> = T extends (...args: infer Args) => infer R ? Args: never
+  type t5 = t4<(arg0: string, arg1: number, arg2: boolean) => void>
+  // t5: [arg0: string, arg1: number, arg2: boolean]
+  type t6 = Reverse<t5>
+  // type t6 = [boolean, number, string]
 }
