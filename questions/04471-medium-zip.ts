62845c82bb7a06a9d5a38a86c7f29ba417de9967
ManfredHu <manfredHuFE@gmail.com>
2022-11-23 15:21:36 +0800

    feat: add zip

---
+++ questions/04471-medium-zip.ts
@@ -0,0 +1,23 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+]
+
+
+// ============= Your Code Here =============
+type Zip<T, U> = T extends [infer F, ...infer R] 
+                ? U extends [infer N, ...infer M]
+                  ? [[F, N], ...Zip<R, M>]
+                  : []
+                : []
+
+// 知识点
+namespace t4471 {
+  // 1. 题意是两两组合，所以都取第一个即可，剩下的递归即可
+}
