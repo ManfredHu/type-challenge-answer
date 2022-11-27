56d1bada822bbfe1257d41242401fa0c9dae5764
ManfredHu <manfredHuFE@gmail.com>
2022-11-27 22:53:56 +0800

    feat: add trim right

---
+++ questions/04803-medium-trim-right.ts
@@ -0,0 +1,19 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+import { Space } from './00106-medium-trimleft'
+
+type cases = [
+  Expect<Equal<TrimRight<'str'>, 'str'>>,
+  Expect<Equal<TrimRight<'str '>, 'str'>>,
+  Expect<Equal<TrimRight<'str     '>, 'str'>>,
+  Expect<Equal<TrimRight<'     str     '>, '     str'>>,
+  Expect<Equal<TrimRight<'   foo bar  \n\t '>, '   foo bar'>>,
+  Expect<Equal<TrimRight<''>, ''>>,
+  Expect<Equal<TrimRight<'\n\t '>, ''>>,
+]
+
+
+// ============= Your Code Here =============
+type TrimRight<S extends string> = S extends `${infer F}${Space}` ? TrimRight<F>: S
+
+//
